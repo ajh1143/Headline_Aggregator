6ccd1aa72ffdb81f0d2cfa3e7d19024c11f16976
ajh1143 <15341747+ajh1143@users.noreply.github.com>
2017-08-11 17:36:11 -0700

    Update Headlines.py
    Added popupgif() method, alerts the user to headline status for Drudge headlines, if text color = ''red'': pop up w/ red text of headline, if text color != ''red'': pop up w/ white text of headline

--- Headlines.py
+++ Headlines.py
@@ -9,30 +9,47 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+def popupgif(headline, happeningStatus):
+
+    #create the canvas, size in pixels
+    canvas = tkinter.Canvas(width=450, height=300, bg='black')
+    canvas.pack(expand=tkinter.YES, fill=tkinter.BOTH)
+    if happeningStatus:
+        gifH = tkinter.PhotoImage(file='#path to image')
+        canvas.create_image(50, 10, image=gifH, anchor=tkinter.NW)
+        headtext = canvas.create_text(70, 10, text=headline, anchor='nw', font=("Times New Roman", 10, 'bold'),
+                                      fill='red')
+        canvas.insert(headtext, 12, "")
+        tkinter.mainloop()
+    else:
+        canvas.image = ImageTk.PhotoImage(file='#path to image')
+        canvas.create_image(50, 10, image=canvas.image, anchor=tkinter.NW)
+        headtext = canvas.create_text(70, 10, text=headline, anchor='nw', font=("Times New Roman", 10, 'bold'),
+                                      fill='white')
+        canvas.insert(headtext, 12, "")
+        tkinter.mainloop()
+
 def DrudgeScrape():
-  #full page screenshot generator
-  driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1']) # or add to your PATH
-  driver.get('https://www.drudgereport.com/')
-  driver.save_screenshot("FullPage_Screenshot.png")
-
-  #headline - Acquires top story, checks if "red", prints status based on color
-  req = requests.get('http://www.drudgereport.com')
-  soup = BeautifulSoup(req.content, 'html.parser')
-  div = soup.find(id="app_mainheadline")
-  stringed = str(div)
-  RedHeadLine = div.find_all("font", attrs={'color': 'red'})
-  if RedHeadLine:
-      print("Important Front Page Story:\n"+div.get_text().strip()+"\n")
-  else:
-      print("Front Page Story: " + div.get_text())
-
-  #redlines - Checks the rest of the stories for "red" status, prints matches
-  print("Here are the rest of the red-status headlines:")
-  div2 = soup.find_all("font", attrs={'color': 'red'})
-  for i in div2:
-      otherheadlines = i.get_text().strip()
-      if str(otherheadlines) != str(div.get_text().strip()):
-          print(i.get_text().strip()+"\n")
+    #headline
+    req = requests.get('http://www.drudgereport.com')
+    soup = BeautifulSoup(req.content, 'html.parser')
+    div = soup.find(id="app_mainheadline")
+    topheadline = div.find_all("font", attrs={'color': 'red'})
+    if topheadline:
+        happening = True
+        print("HAPPENING!\n"+div.get_text().strip()+"\n")
+        popupgif(div.get_text().strip(), happening);
+    else:
+        happening = False
+        print("NOT happening, but here is the headline:\n" + div.get_text()+"\n")
+        popupgif(div.get_text().strip(), happening);
+    #redlines
+    print("Here are the rest of the red-status headlines:")
+    div2 = soup.find_all("font", attrs={'color': 'red'})
+    for i in div2:
+        otherheadlines = i.get_text().strip()
+        if str(otherheadlines) != str(div.get_text().strip()):
+            print(i.get_text().strip()+"\n")
 
 def CnnScrape():
   #full page screenshot generator
